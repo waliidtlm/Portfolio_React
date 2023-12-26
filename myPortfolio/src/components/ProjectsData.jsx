3b9c3ab7f054d943a1386c25c6f7d0e8c65dc225
walidtlm <tlemcani.waliid@gmail.com>
2023-12-26 21:45:54 +0100

    new changes in the project section

---
+++ myPortfolio/src/components/ProjectsData.jsx
@@ -0,0 +1,25 @@
+import Ecommerce from "../assets/Ecommerce.png"
+
+export const ProjectsData = [
+  {
+    name: "Dijkstra Algorithm Visualizer",
+    image: Ecommerce,
+    skills: "JavaScript,HTML,CSS",
+  },
+  {
+    name: "Ecommerce Website",
+    image: Ecommerce,
+    skills: "React,Node.js,MongoDB",
+  },
+  {
+    name: "Spotify Clone",
+    image: Ecommerce,
+    skills: "React,Node.js,MongoDB,SpotifyAPI",
+  },
+  {
+    name: "Social Media Website",
+    image: Ecommerce,
+    skills: "React,Node.js,MySQL,GraphQL",
+  },
+
+];
--- myPortfolio/src/pages/Projects.jsx
+++ myPortfolio/src/pages/Projects.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import ProjectItem from "../components/ProjectItem";
+import { ProjectsData } from "../components/ProjectsData";
+
+import "../styles/Projects.css";
 
 function Projects() {
   return (
-    <div>Projects</div>
-  )
+    <div className="projects">
+      <h1> My Personal <span>Projects</span> </h1>
+      <div className="ProjectData">
+        {ProjectsData.map((project, idx) => {
+          return (
+            <ProjectItem id={idx} name={project.name}  image={project.image}  />
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
-export default Projects
+export default Projects;
