8f4c7ed957d993c7f0fd03d46e85fec027a82821
walidtlm <tlemcani.waliid@gmail.com>
2024-01-12 14:43:45 +0100

    navbar fix

--- myPortfolio/src/components/ProjectsData.jsx
+++ myPortfolio/src/components/ProjectsData.jsx
@@ -7,25 +7,29 @@ export const ProjectsData = [
     name: "Ecommerce Website",
     image: Ecommerce,
     skills: "JavaScript, Mongodb, ReactJS, Express, NodeJS ",
-    githubLink : 'https://github.com/waliidtlm/devmasters'
+    githubLink : 'https://github.com/waliidtlm/devmasters',
+    link:"https://devmasters.ma/"
   },
   {
     name: "Weather App + World time Clock",
     image: weatherApp,
     skills: "React, NodeJS, JavaScript",
-    githubLink : 'https://github.com/waliidtlm/weather-app/tree/main/weather%20app'
+    githubLink : 'https://github.com/waliidtlm/weather-app/tree/main/weather%20app',
+    link: "https://my-weather-net.netlify.app/"
   },
   {
     name: "Spotify Clone",
     image: Ecommerce,
     skills: "React,Node.js,MongoDB,SpotifyAPI",
-    githubLink : 'https://github.com/waliidtlm/weather-app/tree/main/weather%20app'
+    githubLink : 'https://github.com/waliidtlm/weather-app/tree/main/weather%20app',
+    link: "https://my-weather-net.netlify.app/"
   },
   {
     name: "Social Media Website",
     image: Ecommerce,
     skills: "React,Node.js,MySQL,GraphQL",
-    githubLink : 'https://github.com/waliidtlm/weather-app/tree/main/weather%20app'
+    githubLink : 'https://github.com/waliidtlm/weather-app/tree/main/weather%20app',
+    link: "https://my-weather-net.netlify.app/"
   },
 
 ];
--- myPortfolio/src/pages/ProjectDisplay.jsx
+++ myPortfolio/src/pages/ProjectDisplay.jsx
@@ -12,19 +12,23 @@ function ProjectDisplay() {
   return (
     <div className="project">
       <h1> {project.name}</h1>
-      <div className="project-img">
+      <div  className="project-img">
         <img src={project.image} />
       </div>
       <p>
         <b>Skills:</b> {project.skills}
       </p>
-      
-      <a href={project.githubLink}>
-      <FontAwesomeIcon icon={faGithub} />
+
+      <div className="icons">
+      <a href={project.githubLink} target="_blank">
+      <FontAwesomeIcon icon={faGithub} style={{color: "#ededed",}}  className="github"/>
       </a>
-      <a href={project.githubLink}>
-      <FontAwesomeIcon icon={faLink} style={{color: "#ededed",}} />     
+
+      <a href={project.link} target="_blank">
+      <FontAwesomeIcon icon={faLink} style={{color: "#ededed",}} className="link" />     
       </a>
+      </div>
+
     </div>
   );
 }
